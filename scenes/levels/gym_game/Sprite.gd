5d558fbeea6c66dd202e7b653492beeee1be4bf8
BryonnaC <bryonnacrowley@gmail.com>
2023-03-20 21:57:17 -0400

    block input...kinda?

--- scenes/levels/gym_game/Sprite.gd
+++ scenes/levels/gym_game/Sprite.gd
@@ -1,5 +1,3 @@
-class_name JumpBar
-
 extends Sprite2D
 
 var speed = 400
@@ -13,20 +11,25 @@ func _input(event):
 		if move_at_all == true:
 			if position.y > 160 and position.y < 240:
 				move_at_all = false
+				#Global.set_player_has_control(false)
 				
 	elif event.is_action_pressed("ui_up"):
 		if move_at_all == false:
+			Global.set_player_has_control(true)
 			move_at_all = true
+
 
 func _process(delta):
 	var direction = 1
 	
 	if move_at_all == true:
+		Global.set_player_has_control(true)
 		if move_up == true:
 			position.y -= angular_speed + direction * delta
 			if position.y > 90 and position.y < 110:
 				move_up = false
 		elif move_up == false:
+			Global.set_player_has_control(false)
 			position.y += angular_speed + direction * delta
 			if position.y > 290 and position.y < 310:
 				move_up = true
