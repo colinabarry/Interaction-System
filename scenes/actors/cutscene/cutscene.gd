76406fc3b4f02b98927a3a5c5d493ffa61ed2c32
Colin Barry <colin.andrew.barry@gmail.com>
2023-03-21 20:04:21 -0400

    cutscene comments, slightly better terrain texture

--- scenes/actors/cutscene/cutscene.gd
+++ scenes/actors/cutscene/cutscene.gd
@@ -2,17 +2,23 @@
 class_name Cutscene
 extends Node
 
+## A node to create cutscenes
+##
+## Drag-and-drop to instance this node in the editor. It will create an
+## AnimationPlayer as a child of itself. Use the AnimationPlayer to create as
+## many animations as desired, which will be played in sequence.
+
+signal cutscene_ended(cutscene_name: String)
+
 const animation_player_name := "AnimPlayer"
 
 @export var remove_player_control := true
 @export var start_on_ready := false
-@export var auto_queue_animations := true
+# @export var auto_queue_animations := true # TODO: Implement "false"
 
 var animation_player: AnimationPlayer
 
 
-# when a new cutscene is instanced in the editor, it will give itself an
-# AnimationPlayer outside of its own scene so you can add your animations
 func _enter_tree() -> void:
 	# don't create the anim player again when the game runs
 	animation_player = get_node_or_null(animation_player_name)
@@ -32,6 +38,7 @@ func _ready():
 		start()
 
 
+## Starts the cutscene. Removes player control (if applicable) and plays animation queue.
 func start():
 	if remove_player_control:
 		Global.set_player_has_control(false)
@@ -41,7 +48,10 @@ func start():
 		animation_player.queue(anim)
 
 
+## Called when the animation queue is finished.
 func end():
+	cutscene_ended.emit(name)
+
 	if remove_player_control:
 		Global.set_player_has_control(true)
 
