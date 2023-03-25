8eb773883fb03ba9b886da8de87f874b3089ed9b
SaberTothJoe <74932966+SaberTothJoe@users.noreply.github.com>
2023-03-24 21:46:27 -0400

    Gym scene change

--- scenes/levels/overworld/DoorGymO.gd
+++ scenes/levels/overworld/DoorGymO.gd
@@ -1,24 +1,23 @@
 extends Area3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 var entered = false
 
 
 
 func _on_body_entered(body: PhysicsBody3D):
-	entered = true
-
-
+	if body.is_in_group("player"):
+		entered = true
+		print("entered")
+	
 
 func _on_body_exited(body):
-	entered = false
+	if body.is_in_group("player"):
+		print("exited")
+		entered = false
 
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene_to_file("res://scenes/levels/gym_game/jump_2d.tscn")
+	if entered:
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene_to_file("res://scenes/actors/game_base/game_base.tscn")
