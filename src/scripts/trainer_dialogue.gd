1d35168fa36ee6ecd8109db0148029846ecd2ada
BryonnaC <bryonnacrowley@gmail.com>
2023-03-28 06:37:12 -0400

    beginning of trainer dialogue
    should it get more technical? idk its for kids

---
+++ src/scripts/trainer_dialogue.gd
@@ -0,0 +1,23 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+const config ={
+	"speaker":"Trainer Berry",
+	"using_typing": true,
+	"phrases":
+		[
+			"Alright kid, now it's time for jump training.",
+			"This little bar on the side will show you when to jump.",
+			"You must do your best to maintain an appropriate posture and only jump when you're putting the least strain on your body.",
+			"Of course, jumping isn't the concern. How you land from your jump is what causes injury, which is why we want to make sure you're always prepared with proper posture."
+		]
+}
