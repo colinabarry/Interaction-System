504d932c07900f9a2641f4fd98215a35e19fba58
ghgsrt <ajb7366@psu.edu>
2023-03-29 13:52:50 -0400

    Refactor

--- src/dialogues/dialogue_resource.gd
+++
@@ -1,7 +0,0 @@
-class_name DialogueResource extends Resource
-
-
-func _init(_resource_name: String):
-	if Engine.is_editor_hint():
-		var res := load("res://src/dialogues/%s.gd" % _resource_name)
-		ResourceSaver.save(res, "res://assets/dialogues/%s.tres" % _resource_name)
