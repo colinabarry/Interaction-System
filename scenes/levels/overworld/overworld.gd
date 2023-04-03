f80d1c5bb06acf8c2e9bd1d024284c41277da92d
Colin Barry <colin.andrew.barry@gmail.com>
2023-04-03 17:20:01 -0400

    misc

--- scenes/levels/overworld/overworld.gd
+++ scenes/levels/overworld/overworld.gd
@@ -11,8 +11,8 @@ func _ready():
 	Global.unpaused.connect(_on_unpaused)
 
 	if Global.progress_state == Global.PROGRESS_STATE.GAME_STARTED:
-		pass
-		# cutscene.start()
+		cutscene.start()
+		# pass
 
 
 func _on_unpaused():
