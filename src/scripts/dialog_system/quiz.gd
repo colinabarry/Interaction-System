6c77570dcacdcb6b03da41f656c273915bfcc8ba
ghgsrt <ajb7366@psu.edu>
2023-04-04 01:52:02 -0400

    Better Dialogue

--- src/scripts/dialog_system/quiz.gd
+++ src/scripts/dialog_system/quiz.gd
@@ -53,10 +53,21 @@ const PROPAGATED_PROPERTIES = ["speaker", "using_typing"]
 # the default phrases for the `start` and `end` Dialogs, as well as the
 # "correct" and "wrong" answer cases
 var _const_phrases := {
-	"start": ["Hello sweetie:)", "Time for a QUIZ!!!"],
-	"correct": ["WOOOOO THAT'S CORRECT!!"],
-	"wrong": ["I'm sorry, that's incorrect"],
-	"end": ["QUIZ OVER!", "You scored %s/%s!!!"],
+	"start":
+	[
+		"Welcome back! We're so proud of your progress during rehab. It seems like you've learned a lot about the ACL and how to take care of your knee.",
+		"To see how much you've learned, we thought it would be fun to have a little quiz about the ACL. Don't worry, it's just a friendly way to test your knowledge.",
+		"Ready? Let's start the quiz!"
+	],
+	"correct": ["Nice job, that's correct! You're really getting the hang of this."],
+	"wrong": ["Oops, that's not quite right. But don't worry, everyone makes mistakes."],
+	"end":
+	[
+		"Great job on the quiz! You've really gained a lot of knowledge about the ACL and how to prevent future injuries.",
+		"You scored %s/%s!!!",
+		"Remember to keep practicing what you've learned and stay consistent with your exercises. Your dedication will help ensure a healthy and active lifestyle!",
+		"We're always here to support you. Keep up the fantastic work!"
+	],
 }
 
 # the head of the quiz's Sequence
