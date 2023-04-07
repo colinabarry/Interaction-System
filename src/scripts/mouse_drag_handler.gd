c5ad85bde083b0c284fec1ba097bcc313c708c3a
Colin Barry <colin.andrew.barry@gmail.com>
2023-04-06 23:00:56 -0400

    create mouse_drag_handler.gd

---
+++ src/scripts/mouse_drag_handler.gd
@@ -0,0 +1,32 @@
+class_name MouseDragHandler extends StaticBody3D
+
+var can_drag := false
+var is_dragging := false
+var mouse_motion: InputEventMouseMotion
+
+
+func _process(_delta: float) -> void:
+	if can_drag and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		is_dragging = true
+	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		is_dragging = false
+
+
+func _input(event: InputEvent) -> void:
+	mouse_motion = event as InputEventMouseMotion
+
+	if mouse_motion and not Global.is_paused and is_dragging:
+		_drag()
+
+
+## Implement this method. It is called by _input while being dragged.
+func _drag() -> void:
+	pass
+
+
+func _mouse_enter() -> void:
+	can_drag = true
+
+
+func _mouse_exit() -> void:
+	can_drag = false
