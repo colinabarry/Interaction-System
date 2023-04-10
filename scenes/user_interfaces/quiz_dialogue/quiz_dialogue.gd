ec40ad86fc8e6365c4faad2eb48f487a73558401
Colin Barry <colin.andrew.barry@gmail.com>
2023-04-10 13:37:48 -0400

    show quiz on ready

--- scenes/user_interfaces/quiz_dialogue/quiz_dialogue.gd
+++ scenes/user_interfaces/quiz_dialogue/quiz_dialogue.gd
@@ -3,9 +3,16 @@ extends DialogueSystem
 
 var active_sequence := 0
 
+
+func _ready() -> void:
+	super()
+	show_box()
+	try_begin_dialogue()
+
+
 func _on_seq_cold():
-    super()
-    active_sequence += 1
-    active_sequence %= 4
-    print("CHANGING SEQUENCE")
-    change_sequence(active_sequence)
+	super()
+	active_sequence += 1
+	active_sequence %= 4
+	print("CHANGING SEQUENCE")
+	change_sequence(active_sequence)
