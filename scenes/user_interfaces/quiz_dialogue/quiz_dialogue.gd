e87855908398166f96932011261df93752cb4015
Alex Bosco <ajbosco00@gmail.com>
2023-04-12 19:50:00 -0400

    Return to start menu after quiz ends

--- scenes/user_interfaces/quiz_dialogue/quiz_dialogue.gd
+++ scenes/user_interfaces/quiz_dialogue/quiz_dialogue.gd
@@ -10,9 +10,14 @@ func _ready() -> void:
 	try_begin_dialogue()
 
 
-func _on_seq_cold():
+# func _on_seq_cold():
+# 	super()
+# 	active_sequence += 1
+# 	active_sequence %= 4
+# 	print("CHANGING SEQUENCE")
+# 	change_sequence(active_sequence)
+
+
+func _on_seq_dead():
 	super()
-	active_sequence += 1
-	active_sequence %= 4
-	print("CHANGING SEQUENCE")
-	change_sequence(active_sequence)
+	get_tree().change_scene_to_file("res://scenes/user_interfaces/start_menu/start_menu.tscn")
