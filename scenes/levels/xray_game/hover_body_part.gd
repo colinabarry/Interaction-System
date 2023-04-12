421a873f2d1883020224c071bee5f5bf8ac5b9f5
Colin Barry <colin.andrew.barry@gmail.com>
2023-04-12 12:01:06 -0400

    hover_body_part improvements beginning

--- scenes/levels/xray_game/hover_body_part.gd
+++ scenes/levels/xray_game/hover_body_part.gd
@@ -1,17 +1,25 @@
 class_name HoverBodyPart extends MouseDragHandler
 
-@onready var mesh: MeshInstance3D = $Mesh
+@onready var mesh: MeshInstance3D = (
+	get_children().filter(func(child): return child is MeshInstance3D).front()
+)
 
 
 func _ready() -> void:
-	mesh.mesh.material.albedo_color.a = 0.39
+	mesh.mesh.surface_get_material(0).albedo_color.a = 0.39
 
 
-func _mouse_enter():
-	super()
-	mesh.mesh.material.albedo_color.a = 1
+func highlight() -> void:
+	mesh.mesh.surface_get_material(0).albedo_color.a = 1
 
 
-func _mouse_exit():
-	super()
-	mesh.mesh.material.albedo_color.a = 0.39
+func un_highlight() -> void:
+	mesh.mesh.surface_get_material(0).albedo_color.a = 0.39
+
+# func _mouse_enter():
+# 	super()
+# 	mesh.mesh.surface_get_material(0).albedo_color.a = 1
+
+# func _mouse_exit():
+# 	super()
+# 	mesh.mesh.surface_get_material(0).albedo_color.a = 0.39
--- scenes/levels/xray_game/xray_game.gd
+++ scenes/levels/xray_game/xray_game.gd
@@ -1,1 +1,22 @@
-extends Node
+extends Node3D
+
+const RAY_LENGTH = 10.0
+
+var last_result := {}
+
+
+func _physics_process(_delta: float) -> void:
+	var space_state := get_world_3d().direct_space_state
+	var camera := $GameBase/Camera
+	var mouse_position := get_viewport().get_mouse_position()
+	var from: Vector3 = camera.project_ray_origin(mouse_position)
+	var to: Vector3 = from + camera.project_ray_normal(mouse_position) * RAY_LENGTH
+	var collision_mask := 0b1000
+	var query := PhysicsRayQueryParameters3D.create(from, to, collision_mask)
+
+	var result := space_state.intersect_ray(query)
+	# TODO: do some kind of mouse_enter, mouse_exit logic on this stuff
+	# potentially switching to a long ol cylinder or line or something to just be able to use entered and exited signals hmmmm...
+
+	if result.collider is HoverBodyPart:
+		pass
