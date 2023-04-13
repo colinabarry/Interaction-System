8b796b993558d276278e95ad6f9811422cd7e3ae
Alex Bosco <ajbosco00@gmail.com>
2023-04-12 21:14:54 -0400

    Character selection is marginally better

--- scenes/user_interfaces/character_select/character_select.gd
+++ scenes/user_interfaces/character_select/character_select.gd
@@ -1,18 +1,15 @@
-extends Node3D
+extends Control
 
 @export var character_name: String
-# @export var character: NodePath
 
-signal character_selected(character: NodePath)
+signal character_selected(character: String)
 
-@onready var name_label: Label = $Control/MarginContainer/VBoxContainer/Label
-@onready var character_anchor: Node3D = $CharacterAnchor
+@onready var name_label: Label = $MarginContainer/VBoxContainer/Label
 
 
 func _ready():
 	name_label.text = character_name
-	# character_anchor.add_child(get_node(character))
 
 
-# func _on_button_pressed():
-	# emit_signal("character_selected", character)
+func _on_select_character_pressed():
+	emit_signal("character_selected", character_name)
--- scenes/user_interfaces/character_select_screen/character_select_screen.gd
+++ scenes/user_interfaces/character_select_screen/character_select_screen.gd
@@ -1,1 +1,32 @@
 extends Node
+
+@onready var character_container: Node = $MarginContainer/VBoxContainer/CharacterContainer
+
+var selected_character := ""
+
+var _modulate
+
+
+func _init():
+	_modulate = self.modulate
+	self.modulate = Color.TRANSPARENT
+
+
+func _ready():
+	Global.tween_cubic_modulate(self, _modulate)
+
+	for character in character_container.get_children():
+		character.connect(
+			"character_selected", func(_character: String): selected_character = _character
+		)
+
+
+func _on_start_game_pressed():
+	if selected_character == "":
+		return
+
+	print("Starting game with character: " + selected_character)
+
+	await Global.tween_cubic_modulate(self).finished
+
+	# TODO: start game with selected character
