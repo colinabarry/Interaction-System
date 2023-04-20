b174de94662f56b41d0a2ad8ebc7240d0978bf4d
ghgsrt <ajbosco00@gmail.com>
2023-04-20 09:15:46 -0400

    time skip!!!

--- scenes/actors/game_base/game_base.gd
+++ scenes/actors/game_base/game_base.gd
@@ -4,11 +4,18 @@ extends Node
 @export var end_state := Global.PROGRESS_STATE.GAME_STARTED
 
 @onready var frame: Node3D = $Frame
+@onready var time_skip: Label = $TimeSkip/Label
+
+var _modulate
 
 
 func _ready():
+	
+	_modulate = time_skip.modulate
+	time_skip.modulate = Color.TRANSPARENT
+	
 	Global.show_mouse()
-
+	
 	frame.visible = show_frame
 
 
@@ -16,6 +23,12 @@ func _on_button_pressed() -> void:
 	Global.set_progress_state(end_state)
 	Global.player_has_control = true
 	Global.transition_rect.fade_out()
-
 	await Global.transition_rect.faded_out
+	
+	print(Global.progress_state)
+	if Global.progress_state == Global.PROGRESS_STATE.HOSPITAL_COMPLETED:
+		await Global.tween_cubic_modulate(time_skip, _modulate).finished
+		await create_tween().tween_callback(func(): return false).set_delay(1.5).finished
+		await Global.tween_cubic_modulate(time_skip).finished
+	print("hi")
 	print(get_tree().change_scene_to_packed(Global.overworld))
