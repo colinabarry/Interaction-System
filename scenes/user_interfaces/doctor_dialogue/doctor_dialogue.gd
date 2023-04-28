3d7630d85436c4a5862e6c1e525cf43db8579a3f
Alex Bosco <ajbosco00@gmail.com>
2023-04-27 23:56:08 -0400

    found acl func

--- scenes/user_interfaces/doctor_dialogue/doctor_dialogue.gd
+++ scenes/user_interfaces/doctor_dialogue/doctor_dialogue.gd
@@ -2,9 +2,11 @@
 extends DialogueSystem
 
 @onready var cutscene: Cutscene = get_tree().current_scene.get_node_or_null("Cutscene")
+@onready var leave_button := $"../GameBase/Control"
 
 
 func _ready() -> void:
+	leave_button.hide()
 	super()
 
 	options_font_size = 20
@@ -12,10 +14,9 @@ func _ready() -> void:
 	dialogs["start"].connect("after_all", cutscene.resume_animation)
 
 
-func _input(event: InputEvent) -> void:
-	if event.is_pressed() and event.as_text() == "BracketLeft" and not dialog_sequence.cold:
+func found_acl() -> void:
+	if dialog_sequence.cold:
 		print("changing sequence")
 		hide_options()
 		change_sequence(1)
-
-	super(event)
+		dialog_sequence.connect("dead", leave_button.show)
