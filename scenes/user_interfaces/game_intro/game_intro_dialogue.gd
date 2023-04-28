7a545c62fb21734f501ebf41074695b3b2cb5d2b
Colin Barry <80341037+colinabarry@users.noreply.github.com>
2023-04-28 04:44:50 -0400

    game intro

--- scenes/user_interfaces/game_intro/game_intro_dialogue.gd
+++ scenes/user_interfaces/game_intro/game_intro_dialogue.gd
@@ -3,13 +3,21 @@ extends DialogueSystem
 
 @onready var cutscene: Cutscene = get_tree().current_scene.get_node_or_null("Cutscene")
 
+var dialog_sequence_idx := 0
+
 
 func _ready():
 	super()
 
-	dialogs["p_d_1"].connect("before_all", cutscene.resume_animation)  # player falls down, blows out ACL
-	dialogs["p_t_2"].connect("after_all", cutscene.resume_animation)  # player calls for help
-	# dialogs["p_d_2"].connect("after_all", load_hospital)  # transition to doctor
+	connect("dialog_sequence_changed", increment_seq_idx)
 
-# func load_hospital():
-# 	get_tree().change_scene_to_file("scenes/levels/xray_game/xray_game.tscn")
+	dialog_sequence.connect("dead", cutscene.resume_animation)
+
+
+# super scuffed but whatever
+func increment_seq_idx():
+	dialog_sequence_idx += 1
+
+	if dialog_sequence_idx == 1:
+		# dialogs["n_post_tear"].connect("after_all", cutscene.resume_animation)  #
+		dialog_sequence.connect("dead", cutscene.resume_animation)
