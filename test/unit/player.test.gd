9a889a8486d1fe2c2a4d619f66ca997c9a740853
Alex Bosco <ajbosco00@gmail.com>
2023-05-01 14:33:55 -0400

    test

--- test/unit/player.test.gd
+++ test/unit/player.test.gd
@@ -10,73 +10,73 @@ extends GutTest
 # TODO: assign player from the node within the overworld scene
 #		rather than creating a duplicate player
 
-const OVERWORLD_SCENE_PATH = "res://scenes/levels/overworld/test_world.tscn"
+# const OVERWORLD_SCENE_PATH = "res://scenes/levels/overworld/test_world.tscn"
 
-var overworld = preload(OVERWORLD_SCENE_PATH).instantiate()  # debugging view
-var sender = InputSender.new(Input)
-
-
-func _cleanup():
-	sender.release_all()
-	sender.clear()
+# var overworld = preload(OVERWORLD_SCENE_PATH).instantiate()  # debugging view
+# var sender = InputSender.new(Input)
 
 
-func before_all():
-	Global.add_child(overworld)  #!dbg
-	gut.p("Player:Unit->START\n", 2)
+# func _cleanup():
+# 	sender.release_all()
+# 	sender.clear()
 
 
-func after_all():
-	gut.p("Player:Unit->END\n", 2)
+# func before_all():
+# 	Global.add_child(overworld)  #!dbg
+# 	gut.p("Player:Unit->START\n", 2)
 
 
-func after_each():
-	gut.p("Cleanup Inputs", 2)
-	_cleanup()
+# func after_all():
+# 	gut.p("Player:Unit->END\n", 2)
 
 
-const dir_map = {
-	"+x": "left",
-	"-x": "right",
-	"-y": null,
-	"+y": "jump",
-	"-z": "backward",
-	"+z": "forward",
-}
-var assert_map = {"-": assert_gt, "+": assert_lt}
+# func after_each():
+# 	gut.p("Cleanup Inputs", 2)
+# 	_cleanup()
 
 
-func assert_move(dir: String, hold_for_s := 2.0, wait_s := 2.5) -> void:
-	var player = overworld.get_node("Player") as NewPlayer
-
-	var prev_pos = player.position[dir[1]]
-	await sender.action_down("move_" + dir_map[dir]).hold_for(hold_for_s).wait_secs(wait_s)
-	await create_tween().tween_callback(func(): return false).set_delay(wait_s).finished
-	var new_pos = player.position[dir[1]]
-
-	assert_map[dir[0]].call(new_pos, prev_pos)
-
-
-func test_move_left():
-	assert_move("+x")
-	await create_tween().tween_callback(func(): return false).set_delay(3).finished
+# const dir_map = {
+# 	"+x": "left",
+# 	"-x": "right",
+# 	"-y": null,
+# 	"+y": "jump",
+# 	"-z": "backward",
+# 	"+z": "forward",
+# }
+# var assert_map = {"-": assert_gt, "+": assert_lt}
 
 
-func test_move_right():
-	assert_move("-x")
-	await create_tween().tween_callback(func(): return false).set_delay(3).finished
+# func assert_move(dir: String, hold_for_s := 2.0, wait_s := 2.5) -> void:
+# 	var player = overworld.get_node("Player") as NewPlayer
+
+# 	var prev_pos = player.position[dir[1]]
+# 	await sender.action_down("move_" + dir_map[dir]).hold_for(hold_for_s).wait_secs(wait_s)
+# 	await create_tween().tween_callback(func(): return false).set_delay(wait_s).finished
+# 	var new_pos = player.position[dir[1]]
+
+# 	assert_map[dir[0]].call(new_pos, prev_pos)
 
 
-func test_move_forward():
-	assert_move("+z")
-	await create_tween().tween_callback(func(): return false).set_delay(3).finished
+# func test_move_left():
+# 	assert_move("+x")
+# 	await create_tween().tween_callback(func(): return false).set_delay(3).finished
 
 
-func test_move_backward():
-	assert_move("-z")
-	await create_tween().tween_callback(func(): return false).set_delay(3).finished
+# func test_move_right():
+# 	assert_move("-x")
+# 	await create_tween().tween_callback(func(): return false).set_delay(3).finished
 
 
-func test_move_jump():
-	assert_move("+y")
-	await create_tween().tween_callback(func(): return false).set_delay(3).finished
+# func test_move_forward():
+# 	assert_move("+z")
+# 	await create_tween().tween_callback(func(): return false).set_delay(3).finished
+
+
+# func test_move_backward():
+# 	assert_move("-z")
+# 	await create_tween().tween_callback(func(): return false).set_delay(3).finished
+
+
+# func test_move_jump():
+# 	assert_move("+y")
+# 	await create_tween().tween_callback(func(): return false).set_delay(3).finished
